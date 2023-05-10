e74e779f26797d2c3e6caf13bd45b61fa15d4f50
Wang Meng <46134074+Maigewm@users.noreply.github.com>
2023-05-10 21:06:09 +0800

    Update and rename modeling_vilt.py to modeling_afno.py

--- model/modeling_afno.py
+++ model/modeling_afno.py
@@ -1,20 +1,3 @@
-# coding=utf-8
-#-*- coding : utf-8-*-#coding:unicode_escape
-# Copyright 2022 NAVER AI Labs and The HuggingFace Inc. team. All rights reserved.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-""" PyTorch ViLT model."""
-
 import collections.abc
 import math
 from dataclasses import dataclass
@@ -39,7 +22,7 @@ from transformers.pytorch_utils import find_pruneable_heads_and_indices, is_torc
 from transformers.utils import add_start_docstrings, add_start_docstrings_to_model_forward, logging, replace_return_docstrings
 from transformers.models.vilt.configuration_vilt import ViltConfig
 import ipdb 
-from afno import AFNO1D,AFNO2D
+from afno import AFNO1D
 logger = logging.get_logger(__name__)
 
 if not is_torch_greater_or_equal_than_1_10:
@@ -334,127 +317,10 @@ class ViltPatchEmbeddings(nn.Module):
         return x
 
 
-class ViltSelfAttention(nn.Module):
-    def __init__(self, config):
-        super().__init__()
-        if config.hidden_size % config.num_attention_heads != 0 and not hasattr(config, "embedding_size"):
-            raise ValueError(
-                f"The hidden size {config.hidden_size,} is not a multiple of the number of attention "
-                f"heads {config.num_attention_heads}."
-            )
 
-        self.num_attention_heads = config.num_attention_heads
-        self.attention_head_size = int(config.hidden_size / config.num_attention_heads)
-        self.all_head_size = self.num_attention_heads * self.attention_head_size
-
-        self.query = nn.Linear(config.hidden_size, self.all_head_size, bias=config.qkv_bias)
-        self.key = nn.Linear(config.hidden_size, self.all_head_size, bias=config.qkv_bias)
-        self.value = nn.Linear(config.hidden_size, self.all_head_size, bias=config.qkv_bias)
-
-        self.dropout = nn.Dropout(config.attention_probs_dropout_prob)
-
-    def transpose_for_scores(self, x):
-        new_x_shape = x.size()[:-1] + (self.num_attention_heads, self.attention_head_size)
-        x = x.view(*new_x_shape)
-        return x.permute(0, 2, 1, 3)
-
-    def forward(self, hidden_states, attention_mask=None, head_mask=None, output_attentions=False):
-        mixed_query_layer = self.query(hidden_states)
-
-        key_layer = self.transpose_for_scores(self.key(hidden_states))
-        value_layer = self.transpose_for_scores(self.value(hidden_states))
-        query_layer = self.transpose_for_scores(mixed_query_layer)
-
-        # Take the dot product between "query" and "key" to get the raw attention scores.
-        attention_scores = torch.matmul(query_layer, key_layer.transpose(-1, -2))
-        attention_scores = attention_scores / math.sqrt(self.attention_head_size)
-        if attention_mask is not None:
-            # Apply the attention mask is (precomputed for all layers in BertModel forward() function)
-            attention_scores = attention_scores + attention_mask
-
-        # Normalize the attention scores to probabilities.
-        attention_probs = nn.Softmax(dim=-1)(attention_scores)
-
-        # This is actually dropping out entire tokens to attend to, which might
-        # seem a bit unusual, but is taken from the original Transformer paper.
-        attention_probs = self.dropout(attention_probs)
-
-        # Mask heads if we want to
-        if head_mask is not None:
-            attention_probs = attention_probs * head_mask
-
-        context_layer = torch.matmul(attention_probs, value_layer)
-
-        context_layer = context_layer.permute(0, 2, 1, 3).contiguous()
-        new_context_layer_shape = context_layer.size()[:-2] + (self.all_head_size,)
-        context_layer = context_layer.view(*new_context_layer_shape)
-
-        outputs = (context_layer, attention_probs) if output_attentions else (context_layer,)
-        #change
-        #print("self attention output shape",outputs)
-
-        return outputs
-
-
-# Copied from transformers.models.vit.modeling_vit.ViTSelfOutput with ViT->Vilt
-class ViltSelfOutput(nn.Module):
-    """
-    The residual connection is defined in ViltLayer instead of here (as is the case with other models), due to the
-    layernorm applied before each block.
-    """
-
-    def __init__(self, config: ViltConfig) -> None:
-        super().__init__()
-        self.dense = nn.Linear(config.hidden_size, config.hidden_size)
-        self.dropout = nn.Dropout(config.hidden_dropout_prob)
-
-    def forward(self, hidden_states: torch.Tensor, input_tensor: torch.Tensor) -> torch.Tensor:
-
-        hidden_states = self.dense(hidden_states)
-        hidden_states = self.dropout(hidden_states)
-
-        return hidden_states
 
 #CHANGE self attention to afno
 
-class ViltAttention(nn.Module):
-    def __init__(self, config):
-        super().__init__()
-        self.attention = ViltSelfAttention(config)#change self-attention
-        # self.attention = AFNO1D(hidden_size=config.hidden_size, num_blocks=8, sparsity_threshold=0.01, hard_thresholding_fraction=1, hidden_size_factor=1)
-        self.output = ViltSelfOutput(config)
-        self.pruned_heads = set()
-
-    def prune_heads(self, heads):
-        if len(heads) == 0:
-            return
-        heads, index = find_pruneable_heads_and_indices(
-            heads, self.attention.num_attention_heads, self.attention.attention_head_size, self.pruned_heads
-        )
-
-        # Prune linear layers
-        self.attention.query = prune_linear_layer(self.attention.query, index)
-        self.attention.key = prune_linear_layer(self.attention.key, index)
-        self.attention.value = prune_linear_layer(self.attention.value, index)
-        self.output.dense = prune_linear_layer(self.output.dense, index, dim=1)
-
-        # Update hyper params and store pruned heads
-        self.attention.num_attention_heads = self.attention.num_attention_heads - len(heads)
-        self.attention.all_head_size = self.attention.attention_head_size * self.attention.num_attention_heads
-        self.pruned_heads = self.pruned_heads.union(heads)
-
-    def forward(self, hidden_states, attention_mask=None, head_mask=None, output_attentions=False):
-        # print("attention mask",attention_mask,"head mask",head_mask,"output attentions",output_attentions)#change print
-        #ipdb.set_trace()
-        self_outputs = self.attention(hidden_states, attention_mask, head_mask, output_attentions)
-
-        attention_output = self.output(self_outputs[0], hidden_states)
-
-        outputs = (attention_output,) + self_outputs[1:]  # add attentions if we output them
-        #selfoutputs=1
-        #ipdb.set_trace()
-        #print("attention output shape",outputs)
-        return outputs
 
 
 # Copied from transformers.models.vit.modeling_vit.ViTIntermediate with ViT->Vilt
@@ -499,9 +365,7 @@ class ViltLayer(nn.Module):
         self.chunk_size_feed_forward = config.chunk_size_feed_forward
         self.seq_len_dim = 1
         #change
-        #self.attention = AFNO2D(hidden_size=config.hidden_size, num_blocks=8, sparsity_threshold=0.01, hard_thresholding_fraction=1, hidden_size_factor=1)
         self.attention = AFNO1D(hidden_size=config.hidden_size, num_blocks=8, sparsity_threshold=0.01, hard_thresholding_fraction=1, hidden_size_factor=1)
-        #self.attention = ViltAttention(config)
         self.intermediate = ViltIntermediate(config)
         self.output = ViltOutput(config)
         self.layernorm_before = nn.LayerNorm(config.hidden_size, eps=config.layer_norm_eps)
@@ -514,10 +378,7 @@ class ViltLayer(nn.Module):
             #head_mask,
             #output_attentions=output_attentions,#change
         )
-        #change 替换后加ipdb看下输出是否只需要self attention outputs即可
         
-        #attention_output = self_attention_outputs[0]
-        #outputs = self_attention_outputs[1:]  # add self attentions if we output attention weights
         attention_output = self_attention_outputs
         # first residual connection
         hidden_states = attention_output + hidden_states.to(attention_output.device)
@@ -528,10 +389,6 @@ class ViltLayer(nn.Module):
 
         # second residual connection is done here
         layer_output = self.output(layer_output, hidden_states)
-
-        #outputs = (layer_output,) + outputs#change
-
-        #return outputs
         return layer_output
 
 
@@ -576,10 +433,10 @@ class ViltEncoder(nn.Module):
                 )
             else:
                #layer_outputs = layer_module(hidden_states, attention_mask, layer_head_mask, output_attentions)change
-                #print(i, hidden_states.shape)
+              
                 layer_outputs = layer_module(hidden_states, attention_mask, layer_head_mask)
             hidden_states=layer_outputs
-            #hidden_states = layer_outputs[0] #change
+            
 
             if output_attentions:
                 all_self_attentions = all_self_attentions + (layer_outputs[1],)
